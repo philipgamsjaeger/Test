26e1f8ae329ab9acc39a3581d4c607edce8ea708
philip <p.sacherer@gmail.com>
2020-11-18 15:25:27 +0100

    REchnungService

--- src/main/java/at/itk/gamsjaeger/rechnungkunde/repositories/services/RechnungService.java
+++ src/main/java/at/itk/gamsjaeger/rechnungkunde/repositories/services/RechnungService.java
@@ -1,11 +1,16 @@
 package at.itk.gamsjaeger.rechnungkunde.repositories.services;
 
+import at.itk.gamsjaeger.rechnungkunde.models.Kunde;
+import at.itk.gamsjaeger.rechnungkunde.models.Rechnung;
 import at.itk.gamsjaeger.rechnungkunde.repositories.KundeRepository;
 import at.itk.gamsjaeger.rechnungkunde.repositories.RechnungRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -16,4 +21,19 @@ public class RechnungService {
     @Autowired
     KundeRepository kundeRepository;
 
+    public List<Rechnung> getAllRechnungen(){
+        return rechnungRepository.findAll();
+    }
+
+    public Optional<Rechnung> getKundeById(Long id){
+        return rechnungRepository.findById(id);
+    }
+
+    public Rechnung addRechnung (Rechnung rechnung){
+        return rechnungRepository.save(rechnung);
+    }
+
+    public void deleteRechnungByID (Long id){
+        rechnungRepository.deleteById(id);
+    }
 }

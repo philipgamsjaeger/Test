e8ff2803293d4c1966294af0fe3f57d0cd5c1dee
philip <p.sacherer@gmail.com>
2020-11-18 17:25:49 +0100

    findByKunde jetzt wirlich raus

--- src/main/java/at/itk/gamsjaeger/rechnungkunde/repositories/KundeRepository.java
+++ src/main/java/at/itk/gamsjaeger/rechnungkunde/repositories/KundeRepository.java
@@ -3,10 +3,10 @@ package at.itk.gamsjaeger.rechnungkunde.repositories;
 import at.itk.gamsjaeger.rechnungkunde.models.Kunde;
 import at.itk.gamsjaeger.rechnungkunde.models.Rechnung;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface KundeRepository extends JpaRepository<Kunde,Long> {
-    List<Rechnung> findByKunde(Kunde kunde);
 }
